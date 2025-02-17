613cf534de444d8f034fdab5e802328f2d5090bf
dvezzoni <Dinno>
2025-02-17 19:35:56 -0300

    fix hooks

--- src/steps/example.test.ts
+++ src/steps/example.test.ts
@@ -1,22 +1,19 @@
-import { expect } from '@playwright/test';
-import { Given, When, Then } from '@cucumber/cucumber';
-import { BASEURL } from '../config';
-import { page } from '../hooks/hook';
-import { validateFirstLocator } from '../utils/validations';
-import { inputLabel, buttonSearch, divResult } from '../locators/exampleLocators';
-import { getByPlaceholderAndClickIt, getByPlaceholderAndFillIt, getElementByRole } from '../utils/interactions';
+import { Given, When, Then } from "@cucumber/cucumber";
+import { expect } from "@playwright/test";
+import { BASEURL } from "../config";
 
-Given("User navigates to MercadoLibre page", async () => {
-  await page.goto(BASEURL);
+Given("User navigates to MercadoLibre page", async function (this: any) {
+  await this.page.goto(BASEURL);
 });
 
-When('User search for cars options', async function () {
-  await getByPlaceholderAndClickIt(page, inputLabel);
-  await getByPlaceholderAndFillIt(page, inputLabel, "auto");
-  (await getElementByRole(page, "button", buttonSearch));
-  
+When("User search for cars options", async function (this: any) {
+  const searchInput = await this.page.waitForSelector('input[name="as_word"]');
+  await searchInput.fill("autos");
+  await this.page.keyboard.press("Enter");
+  await this.page.waitForLoadState("networkidle");
 });
 
-Then('It should show all the results according to the search', async function () {
-  expect(validateFirstLocator(page, "div", divResult)).toBeTruthy();
+Then("It should show all the results according to the search", async function (this: any) {
+  const results = await this.page.$$(".ui-search-layout__item");
+  expect(results.length).toBeGreaterThan(0);
 });
